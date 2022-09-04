b5bc8557404757dfdc0dd830da52ef9398877e09
Alexis Parave <aparavev@gmail.com>
2022-09-04 05:52:21 -0500

    FEAT: add functionality to implement endpoint to search products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,4 +9,10 @@ class ProductsController < ApplicationController
     @products = @bsale_service.filter_products(@id)
     render json: @products
   end
+
+  def search
+    query = params[:query]
+    @products = @bsale_service.search(query)
+    render json: @products
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "products#index"
   get "products" => "products#index"
   get "categories" => "categories#index"
+  get "search" => "products#search"
 
   resources :categories do
     get "products", to: "products#filter_products"
