1347d7e1949218db9f748c4ceb0a283c541ad49b
Alexis Parave <aparavev@gmail.com>
2022-09-04 05:48:38 -0500

    FEAT: add functionality to implement endpoint to get products filtered by category

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,10 @@ class ProductsController < ApplicationController
     @products = @bsale_service.list_products
     render json: @products
   end
+
+  def filter_products
+    @id = params[:category_id]
+    @products = @bsale_service.filter_products(@id)
+    render json: @products
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  root 'products#index'
-  get 'products' => "products#index"
+  root "products#index"
+  get "products" => "products#index"
+
+  resources :categories do
+    get "products", to: "products#filter_products"
+  end
 end
