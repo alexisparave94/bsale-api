4aa0a2f6f65375894ac63c541aca8f9536ab7f00
Alexis Parave <aparavev@gmail.com>
2022-09-04 05:33:51 -0500

    FEAT: add service to get all products

---
+++ app/services/bsale_service.rb
@@ -0,0 +1,30 @@
+class BSaleService
+  def initialize
+    @host = "mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com"
+    @database = "bsale_test"
+    @username = "bsale_test"
+    @password = "bsale_test"
+    @client = Mysql2::Client.new(
+      host: @host,
+      username: @username,
+      database: @database,
+      password: @password
+    )
+  end
+
+  def list_products
+    @products = @client.query(
+      "SELECT p.id, p.name, p.url_image, p.price, p.discount, c.name as cat_name
+        FROM product as p
+        JOIN category as c
+        ON p.category = c.id
+        ORDER BY p.id;"
+    )
+  rescue StandardError => e
+    Rails.logger.debug e.message
+    @products = e
+  ensure
+    @client&.close
+    Rails.logger.debug "Done."
+  end
+end
